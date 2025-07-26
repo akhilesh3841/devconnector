e2342dcae3a82d00a341c80b534bc48b3ab2963e
Akhilesh Yadav <akhileshyadav3841@gmail.com>
2025-07-26 15:01:20 +0530

    Your message

--- frontend/src/utils/helper.jsx
+++ frontend/src/utils/helper.jsx
@@ -1,8 +1,8 @@
 
 // export const Base_url=location.hostname === "localhost"?"http://localhost:3000":"/api"
 
-// export const Base_url="https://mernproject-rho.vercel.app/"
-export const Base_url="https://devconnectorserver.vercel.app"
+export const Base_url="https://devconnectorser.onrender.com"
+// export const Base_url="https://devconnectorserver.vercel.app"
 
 
 // export const Base_url="/api";
